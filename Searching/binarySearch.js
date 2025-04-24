6ec44950a3d652b88d3313b180109ca1b02a8895
SOURAV-RK <souravrk25@gmail.com>
2025-04-24 11:45:14 +0530

    corrected the error in the binary search

--- Searching/binarySearch.js
+++ Searching/binarySearch.js
@@ -2,7 +2,7 @@
 function binarySearch(arr,target){
     let left = 0
     let right = arr.length -1
-    while(left >= right){
+    while(left <= right){
         let middle = Math.floor((left + right)/2)
         if(arr[middle] === target) return middle
         else if(target < arr[middle]) right = middle-1
